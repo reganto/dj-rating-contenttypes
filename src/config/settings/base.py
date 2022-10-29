c586f08950121c1679bdeb25ab417ab68ae6ebeb
Reganto <tell.reganto@gmail.com>
2022-10-29 13:34:24 +0330

    Update configs

--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.humanize",
     "common.apps.CommonConfig",
     "rates.apps.RatesConfig",
     "posts.apps.PostsConfig",
@@ -62,7 +63,9 @@ ROOT_URLCONF = "config.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            BASE_DIR / "templates",
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
